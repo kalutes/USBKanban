c9015ff63e41c51297715695f464c7e62fce3831
Kameron Lutes <kalutes@purdue.edu>
2017-09-09 21:23:38 -0400

    added firebase

--- app/components/App.js
+++ app/components/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Firebase from 'firebase';
+import reactMixin from 'react-mixin';
+import ReactFireMixin from 'reactfire';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		var firebaseConfig = {
-			apiKey: ,
-			authDomain: ,
-			databaseURL: ,
-			projectId: ,
-			storageBucket: ,
-			messagingSenderId: 
+			apiKey: "AIzaSyBBxovqKSt_mQBtMl9-_MmtgC9H9duBnkI",
+			authDomain: "usbkanban.firebaseapp.com",
+			databaseURL: "https://usbkanban.firebaseio.com",
+			projectId: "usbkanban",
+			storageBucket: "usbkanban.appspot.com",
+			messagingSenderId: "960459714479"
 		};
 
 		Firebase.initializeApp(firebaseConfig);
